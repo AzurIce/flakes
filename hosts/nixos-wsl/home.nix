3a1fc1f2d9be6f7d3f11edade33e0505d9d0b1b1
AzurIce <973562770@qq.com>
2025-07-09 15:27:24 +0000

    update git for wsl

--- hosts/nixos-wsl/home.nix
+++ hosts/nixos-wsl/home.nix
@@ -8,7 +8,7 @@
     ../../home/programs/nvim.nix
 
     # ../../modules/programs/typora/home.nix
-    ../../home/programs/git.nix
+    # ../../home/programs/git.nix
     ../../home/programs/helix.nix
     # ../../home/programs/waybar
   ];
@@ -17,6 +17,30 @@
     username = "${user}";
     homeDirectory = "/home/${user}";
     stateVersion = "24.11";
+  };
+
+  programs.git = {
+    enable = true;
+    userEmail = "973562770@qq.com";
+    userName = "AzurIce";
+    extraConfig = {
+      # core.excludesFile = "~/.config/git/ignore";
+      http.proxy = "http://127.0.0.1:7890";
+      https.proxy = "https://127.0.0.1:7890";
+      safe.directory = "*";
+      credential = {
+        credentialStore = "secretservice";
+        helper = "/mnt/c/Program\ Files\ \(x86\)/Git\ Credential\ Manager/git-credential-manager.exe";
+      };
+    };
+
+    ignores = [
+      ".DS_Store"
+    ];
+
+    lfs.enable = true;
+
+    delta.enable = true;
   };
 
   programs.home-manager.enable = true;
